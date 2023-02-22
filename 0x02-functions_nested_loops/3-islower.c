435e7d554bdd42b4db325fc160a2dcde965a6938
Davidkalid <davidkalid1985@gmail.com>
2023-02-22 10:52:42 -0800

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Prototype: int _islower(int c);
+ * Returns 1 if c is lowercase
+ * Returns 0 otherwise
+ */
+
+int _islower(int c)
+{
+if(c >= 'a'&& c <= 'z')
+return (1);
+else
+return (0);
+}
+
