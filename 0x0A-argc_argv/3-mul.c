a300210ae3db46da19ba1d2a28f8193d2c4d76f3
Davidkalid <davidkalid1985@gmail.com>
2023-03-15 12:51:29 -0700

    al

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -40,16 +40,13 @@ if (f == 0)
 return (0);
 return (n);
 }
-
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: 0 (Success), 1 (Error)
  */
-
 int main(int argc, char *argv[])
-
 {
 int result, num1, num2;
 if (argc < 3 || argc > 3)
